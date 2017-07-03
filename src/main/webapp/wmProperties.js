10a27798da0da4bbe1adf2750e95cee8046612a3
root <root@61949b3d2f95>
2017-07-03 08:51:43 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,13 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "Auto_NcxkIVTEth",
+  "homePage" : "Main",
+  "name" : "Auto_NcxkIVTEth",
+  "platformType" : "WEB",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
